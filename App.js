ff55658e11a30247a44a8760b9a90f4c2bfa32fe
jrhuasi <julianra@gmail.com>
2021-02-15 06:24:40 -0300

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-/**
- * https://github.com/xAgustin93/5-tenedores-react-native
- */
-import React, { useEffect } from "react";
-import Navigation from "./app/navigations/Navigation";
-import { firebaseApp } from "./app/utils/firebase";
-
-import * as firebase from "firebase";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  /* useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      console.log(user);
-    });
-  }, []); */
-  return <Navigation />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
